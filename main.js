0f865d20ac60dedb5509736c9e43f36fd251f471
Shogo Kitano <arsenic28@gmail.com>
2014-04-14 21:44:19 +0900

    表を追加

--- main.js
+++ main.js
@@ -47,18 +47,68 @@ var makePlots = function(solved_list){
 
 var makeDataObject = function(user_id){
     var res = {};
+    // for graph
     res.label = user_id;
     res.data = makePlots(getSolvedProblems(user_id));
-    res.hoverable = true;
+
+    // for table
+    res.id = user_id;
+    res.solved = res.data.length;
+    var last = res.data[res.data.length-1];
+    var first = res.data[0];
+    res.age = last[0] - first[0];
+    res.lastAC = last[0];
+    return res;
+};
+
+var members = [
+    "Respect2D",
+    "yokit9",
+    "utisam",
+    "ik11235",
+    "arsenic28",
+    "bnsgny",
+    "menphim",
+    "dispenser"
+];
+
+var addMember = function(){
+    var user_id = $("#new_user").val();
+    members.push(user_id);
+    updateMembers();
+};
+
+var dtToString = function(dt){
+    var res;
+    if(dt <= 1000*60){
+        res = (dt/1000).toFixed() + "秒";
+    } else if(dt <= 1000*60*60){
+        res = (dt/1000/60).toFixed() + "分";
+    } else if(dt <= 1000*60*60*24){
+        res = (dt/1000/60/60).toFixed() + "時間";
+    } else {
+        res = (dt/1000/60/60/24).toFixed() + "日";
+    }
     return res;
 };
 
 var updateMembers = function(){
-    var members = $("#members").val().split("\n");
     var datas = [];
     for(var i=0; i<members.length; i++){
-        datas.push(makeDataObject(members[i]));
+        var data = makeDataObject(members[i]);
+        datas.push(data);
+        var spd = (data.solved/(data.age/1000/60/60/24)).toFixed(2);
+        var last = dtToString(new Date - data.lastAC) + "前";
+        var age = dtToString(data.age);
+        $("#table").append(
+            $("<tr></tr>")
+                .append($("<td></td>").text(data.id))
+                .append($("<td></td>").text(data.solved))
+                .append($("<td></td>").text(age))
+                .append($("<td></td>").text(spd))
+                .append($("<td></td>").text(last)));
     }
+
     $.plot("#placeholder", datas, {
         xaxis: {
             mode: "time"
